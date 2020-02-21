0c0d7b8ed461203713c1dd71b8e15b0a63e057c4
yaocheng <yaocheng@speakin.mobi>
2020-02-21 17:05:12 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 都君丨大魔王
+import sys
+import os
+import base64
+from picture.icon import img
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import *
+from file_operate import FileOperate
+if hasattr(sys, 'frozen'):
+    os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
+
+with open('tmp.ico', 'wb') as tmp:
+    tmp.write(base64.b64decode(img))
+
+
+class Client(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        '''窗口绘制'''
+        self.setFixedSize(350, 120)
+        self.setWindowTitle('鉴定版本切换工具_20200213')
+        self.setWindowIcon(QtGui.QIcon('tmp.ico'))
+        os.remove('tmp.ico')
+        self.client_grid = QGridLayout(self)
+        self.dev_model_label = QLabel('开发者模式：')
+        self.off_rb = QRadioButton('关闭', self)
+        self.off_rb.setChecked(True)
+        self.on_rb = QRadioButton('打开', self)
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.off_rb, 0)
+        self.button_group.addButton(self.on_rb, 1)
+        self.edition_label = QLabel('请选择版本：')
+        self.edition_combo_box = QComboBox()
+        self.edition_combo_box.addItems(['基础版', '高级版', '专家版'])
+        self.log_info = QLabel()
+        self.change_button = QPushButton('切换(F10)')
+        self.change_button.setShortcut('F10')
+        self.change_button.clicked.connect(self.change_edition)
+        '''元素布局'''
+        self.client_grid.addWidget(self.dev_model_label, 0, 0, 1, 1)
+        self.dev_model_label.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignLeft)
+        self.client_grid.addWidget(self.off_rb, 0, 1, 1, 1)
+        self.client_grid.addWidget(self.on_rb, 0, 2, 1, 1)
+        self.client_grid.addWidget(self.edition_label, 1, 0, 1, 1)
+        self.edition_label.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignLeft)
+        self.client_grid.addWidget(self.edition_combo_box, 1, 1, 1, 1)
+        self.client_grid.addWidget(self.log_info, 2, 0, 1, 3)
+        self.client_grid.addWidget(self.change_button, 3, 0, 3, 3)
+
+    def get_edition_value(self):
+        model_value = self.button_group.checkedId()
+        edition_value = self.edition_combo_box.currentIndex()
+        return model_value, edition_value
+
+    def change_edition(self):
+        model_value, edition_value = self.get_edition_value()
+        self.file_operate_thread = FileOperate(model_value, edition_value)
+        self.file_operate_thread.text.connect(self.print_log)
+        self.file_operate_thread.start()
+
+    def print_log(self, text):
+        self.log_info.setText(text)
+        self.log_info.setStyleSheet("color:blue")
+
+
+def main():
+    app = QApplication(sys.argv)
+    client = Client()
+    client.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ file_operate.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# 都君丨大魔王
+import re
+import winreg
+
+from PyQt5 import QtCore
+
+
+file_content = "/** 0: 基础版, 1: 高级版, 2: 专家版 */\n" \
+               "module.exports = {topLevel: 2,debug: 1,devTools: 1};"
+
+
+class FileOperate(QtCore.QThread):
+    text = QtCore.pyqtSignal(str)
+
+    def __init__(self, model_value, edition_value):
+        super().__init__()
+        self.model_value = model_value
+        self.edition_value = edition_value
+
+    def file_operate(self):
+        # 获取鉴定系统安装磁盘
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall')
+        key_num = winreg.QueryInfoKey(key)[0]
+        finally_disk = ''
+        for a in range(key_num):
+            key_name = winreg.EnumKey(key, a)
+            if key_name.startswith('{') and key_name.endswith('}'):
+                new_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                         f'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{key_name}')
+                new_key_num = winreg.QueryInfoKey(new_key)[1]
+                for b in range(new_key_num):
+                    name, key_type, value = winreg.EnumValue(new_key, b)
+                    if str(key_type).find('Uninstall 国音智能声纹鉴定专家系统.exe') != -1:
+                        finally_disk = key_type[1:2]
+                        break
+        # 鉴定系统版本切换及模式调整
+        edition_info = ['基础版', '高级版', '专家版']
+        model_info = ['关闭', '打开']
+        file_path = f'{finally_disk}:\\Program Files\\voice-identify\\locConf.js'
+        with open(file_path, 'w+', encoding='utf-8') as file:
+            file.read()
+            file.seek(0)
+            file.truncate()  # 清空文件
+            result = re.sub(r'topLevel: (.*?),debug: (.*?),',
+                            f'topLevel: {self.edition_value},debug: {self.model_value},',
+                            file_content)
+            print(result)
+            file.write(result)
+        info = f'开发者模式【{model_info[self.model_value]}】，版本为【{edition_info[self.edition_value]}】，请重启鉴定系统！'
+        self.text.emit(info)
+        return info
+
+    def run(self):
+        self.file_operate()
+
+
+if __name__ == '__main__':
+    file_operate = FileOperate(1, 1)
+    print(file_operate.get_identify_disk())
